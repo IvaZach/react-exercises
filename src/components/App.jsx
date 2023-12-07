56e7854418821830edbb08b4ec8aa37020de2b6f
IvaZach <ivazacherniuk@gmail.com>
2023-12-07 13:28:56 +0300

    add lesson 2(part 3-4)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,10 +14,60 @@
 //     </div>
 //   );
 // };
-import { Card } from './Card/Card'
+// import { Card } from './Card/Card'
 
-const App = () => {
-	return <Card isOnline />
+// const App = () => {
+// 	return <Card isOnline />
+// }
+
+// export default App
+
+
+import { Component } from 'react'
+import Header from './Header/Header'
+import ToDoList from './ToDoList/ToDoList'
+import Modal from './Modal/Modal'
+import FormLogin from './FormLogin/FormLogin'
+
+import { nanoid } from 'nanoid'
+
+class App extends Component {
+	state = {
+		isShowModal: false,
+	}
+
+	showModal = () => {
+		this.setState({ isShowModal: true })
+	}
+
+	closeModal = () => {
+		this.setState({ isShowModal: false })
+	}
+
+	createUser = (data) => {
+		const newUser = {
+			...data,
+			id: nanoid(),
+		}
+		console.log('newUser :>> ', newUser)
+	}
+
+	render() {
+		return (
+			<div className='container'>
+				<Header showModal={this.showModal} />
+				<ToDoList />
+				{this.state.isShowModal && (
+					<Modal closeModal={this.closeModal}>
+						<FormLogin
+							closeModal={this.closeModal}
+							createUser={this.createUser}
+						/>
+					</Modal>
+				)}
+			</div>
+		)
+	}
 }
 
 export default App
